72bc24fe2d1084032e51e7b72374a4ea4e8dcb52
tomoharu.yoshida <tomoharu.yoshida@fixstars.com>
2019-03-14 12:41:11 +0900

    [WIP] make classifier

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,9 +1,16 @@
 from styx_msgs.msg import TrafficLight
+import tensorflow as tf
 
 class TLClassifier(object):
     def __init__(self):
         #TODO load classifier
-        pass
+        detection_graph = tf.Graph()
+        with detection_graph.as_default():
+          od_graph_def = tf.GraphDef()
+          with tf.gfile.GFile(PATH_TO_FROZEN_GRAPH, 'rb') as fid:
+            serialized_graph = fid.read()
+            od_graph_def.ParseFromString(serialized_graph)
+            tf.import_graph_def(od_graph_def, name='')
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
